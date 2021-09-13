a867745e355f4d5f667c6f6a6052e92deeb899f6
ayushij-git <55012987+ayushij-git@users.noreply.github.com>
2021-09-13 11:19:18 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+class Player {
+  private:
+      string _playerName;
+      int _playerId;
+      
+      Player(string playerName, int id) {
+        _playerName  = playerName;
+        _playerId    = id;
+      }
+
+}
